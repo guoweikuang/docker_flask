cc592c47c8d79e90fe36d5ea417851b368dad693
guoweikuang <673411814@qq.com>
2018-11-23 14:47:00 +0800

    remove some

--- app.py
+++ app.py
@@ -1,35 +1,16 @@
 # -*- coding: utf-8 -*-
 import time
 from flask import Flask
+from flask import request
 import random
 
 app = Flask(__name__)
 
 
-from kubernetes import client, config
-import kubernetes
-
-
-def main():
-    # Configs can be set in Configuration class directly or using helper
-    # utility. If no argument provided, the config will be loaded from
-    # default location.
-    #config.load_kube_config()
-    config = kubernetes.client.Configuration()
-    config.host = 'https://10.6.125.6:16443'
-    config.verify_ssl = False
-    
-    v1 = client.CoreV1Api(configuration=config)
-    print("Listing pods with their IPs:")
-    ret = v1.list_pod_for_all_namespaces(watch=False)
-    for i in ret.items:
-        print("%s\t%s\t%s" %
-              (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-
 @app.route('/')
 def index():
-    time.sleep(random.randint(0, 4))
-    # main()
+    print(request.remote_addr)
+    print(request.headers)
     return 'Hello, world, docker flask'
 
 
